30deee082fa87135cbd0b4e11b55c3692065f374
SmokedKoala <k.azarov@inbox.ru>
2021-02-10 23:04:47 +0300

    JSON

--- app/src/main/java/ru/practice/vk_api/MainActivity.java
+++ app/src/main/java/ru/practice/vk_api/MainActivity.java
@@ -2,16 +2,58 @@ package ru.practice.vk_api;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static ru.practice.vk_api.utils.NetworkUtils.generateURL;
+import static ru.practice.vk_api.utils.NetworkUtils.getResponseFromURL;
+
 public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView resultText;
+
+
+    class MyAsyncTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String response){
+            String firstName = null;
+            String lastName = null;
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("response");
+                JSONObject userInfo = jsonArray.getJSONObject(0);
+                firstName = userInfo.getString("first_name");
+                lastName = userInfo.getString("last_name");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            resultText.setText(firstName+" "+lastName);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 switch (v.getId()){
                     case R.id.button_for_search:
-//                        заглушка
-                        resultText.setText("Всё работает");
+                        URL generatedURL = generateURL(searchField.getText().toString());
+                        new MyAsyncTask().execute(generatedURL);
                         break;
                 }
             }
--- app/src/main/java/ru/practice/vk_api/utils/NetworkUtils.java
+++ app/src/main/java/ru/practice/vk_api/utils/NetworkUtils.java
@@ -1,5 +1,60 @@
 package ru.practice.vk_api.utils;
 
-public class NetworkUtils {
+import android.net.Uri;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import static android.content.ContentValues.TAG;
+
+public class NetworkUtils {
+    private static final String VK_API_BASE_URL = "https://api.vk.com";
+    private static final String VK_USERS_GET = "/method/users.get";
+    private static final String PARAM_USER_ID = "user_ids";
+    private static final String PARAM_VERSION = "v";
+    private static final String ACCESS_TOKEN = "access_token";
+
+    public static URL generateURL(String userId)  {
+        Uri builtUri = Uri.parse(VK_API_BASE_URL+VK_USERS_GET)
+                .buildUpon().appendQueryParameter(PARAM_USER_ID, userId)
+                .appendQueryParameter(PARAM_VERSION, "5.89")
+                .appendQueryParameter(ACCESS_TOKEN,
+                        "6edf95646edf95646edf9564c96ea9cf2e66edf6edf95640efd38ef96cb97c3d7dfffd2")
+                .build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+            Log.i(TAG,url.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+;
+    public static String getResponseFromURL(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            //получение входного потока данных
+            InputStream in = urlConnection.getInputStream();
+            //сканнер для считывания данных потока
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
